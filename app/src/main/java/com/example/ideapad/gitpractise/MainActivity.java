16147b504b6e98b7b0e68710aa12d97f2db9ab52
yogesh20 <waniyogeshru2015@gmail.com>
2017-05-24 15:01:42 +0530

    Test changes

--- app/src/main/java/com/example/ideapad/gitpractise/MainActivity.java
+++ app/src/main/java/com/example/ideapad/gitpractise/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.ideapad.gitpractise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//my main project - yogesh
+//my main project -
 public class MainActivity extends AppCompatActivity {
 
     @Override
