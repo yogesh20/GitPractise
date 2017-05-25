a78c4869fd00174bfe688f5a3c574195cd9007f2
yogesh20 <yogesh20@users.noreply.github.com>
2017-05-25 13:57:37 +0530

    Update MainActivity.java
    commmit

--- app/src/main/java/com/example/ideapad/gitpractise/MainActivity.java
+++ app/src/main/java/com/example/ideapad/gitpractise/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        // write code here 
 
     }
 
